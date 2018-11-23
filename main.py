c177b40274a7cd24825dd3fa94e9d12e835f672a
Jdraiv <josuedr11@gmail.com>
2018-11-23 16:21:52 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+# RUN RUN
+
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic()
+
+@app.route('/')
+async def homepage(request):
+    return json({'view': 'homepage'})
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
